94eab4196da2a3e7d1789e984e940ab1e8057873
sstarview <salmankhurshid.me@gmail.com>
2018-11-03 16:43:55 +0530

    Implemented Stack Data Structure

--- StackDataStructure/Program.cs
+++ StackDataStructure/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace StackDataStructure
 {
@@ -10,6 +6,25 @@ namespace StackDataStructure
   {
     static void Main(string[] args)
     {
+      int[] myArray = new int[10];
+
+      Stack stack = new Stack();
+      stack.Push(myArray, 15);
+      stack.Push(myArray, 88);
+      stack.Push(myArray, 98);
+      stack.Push(myArray, 44);
+      stack.Pop(myArray);
+      stack.Push(myArray, 55);
+      stack.Pop(myArray);
+      stack.Pop(myArray);
+      stack.Pop(myArray);
+      stack.Pop(myArray);
+      stack.Pop(myArray);
+      for (int i = 0; i < myArray.Length; i++)
+      {
+        Console.WriteLine(myArray[i]);
+      }
+
     }
   }
 }
---
+++ StackDataStructure/Stack.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace StackDataStructure
+{
+  class Stack
+  {
+    private int arrayTop = -1;
+
+    private bool StackEmpty(int[] array)
+    {
+      if (arrayTop < 0)
+      {
+        return true;
+      }
+      else
+        return false;
+    }
+
+    public void Push(int[] array, int element)
+    {
+      arrayTop = arrayTop + 1;
+      array[arrayTop] = element;
+    }
+
+    public int Pop(int[] array)
+    {
+      if (StackEmpty(array))
+      {
+        throw new Exception("Empty Stack");
+      }
+      else
+      {
+        arrayTop = arrayTop - 1;
+        return array[arrayTop + 1];
+      }
+    }
+  }
+}
